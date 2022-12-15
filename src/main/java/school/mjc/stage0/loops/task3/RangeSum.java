a0e2b52f2949f7fe320c87136020fa3e290c10af
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-15 13:13:12 +0500

    odor

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
-        for (int i = secondBoarder; i >secondBoarder ; i++) {
+        for (int i = firstBoarder; i >secondBoarder ; i++) {
             int sum=+i;
             System.out.println(sum);
 
